b1125511c4c305cd086da646c2d9e509e930a2dd
Kris Cagle <Kriscagle@gmail.com>
2026-05-04 11:14:30 -0500

    added isNullorWhiteSpace

--- ThrownForALoop/Program.cs
+++ ThrownForALoop/Program.cs
@@ -1,6 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-string greeting = @"Welcome to Thrown For a Loop Your one-stop shop for used sporting equipment";
+﻿string greeting = @"Welcome to Thrown for a Loop
+Your one-stop shop for used sporting equipment";
+
 Console.WriteLine(greeting);
 Console.WriteLine("Please enter a product name: ");
+
 string response = Console.ReadLine();
-Console.WriteLine(@$"You chose: {response}. Thank you for your input!");
+
+while (string.IsNullOrWhiteSpace(response))
+{
+    Console.WriteLine("You didn't choose anything, try again!");
+    response = Console.ReadLine().Trim();
+}
+
+Console.WriteLine($"You chose: {response}");
