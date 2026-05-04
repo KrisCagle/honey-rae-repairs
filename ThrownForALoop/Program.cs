52b1a9abf8cb96cbef4492de79bd234ddf709d13
Kris Cagle <Kriscagle@gmail.com>
2026-05-04 11:35:18 -0500

    added integer function

--- ThrownForALoop/Program.cs
+++ ThrownForALoop/Program.cs
@@ -2,14 +2,18 @@
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
-Console.WriteLine("Please enter a product name: ");
+Console.WriteLine(@"Products:
+1. Football
+2. Hockey Stick
+3. Boomerang
+4. Frisbee
+5. Golf Putter");
+Console.WriteLine("Please enter a product number: ");
+int response = int.Parse(Console.ReadLine().Trim());
 
-string response = Console.ReadLine();
-
-while (string.IsNullOrWhiteSpace(response))
+while (response > 5 || response < 1)
 {
-    Console.WriteLine("You didn't choose anything, try again!");
-    response = Console.ReadLine().Trim();
+    Console.WriteLine("Choose a number between 1 and 5!");
+    response = int.Parse(Console.ReadLine().Trim());
 }
 
-Console.WriteLine($"You chose: {response}");
